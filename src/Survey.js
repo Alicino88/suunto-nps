65cc0aeb8b374cf1379eed988d85a35e5cb9006c
Alice Moretti <ali.moret88@gmail.com>
2023-03-02 14:59:34 +0200

    Added basic for structure

--- src/Survey.js
+++ src/Survey.js
@@ -5,14 +5,77 @@ import { Survey } from "survey-react-ui";
 const surveyJson = {
   elements: [
     {
-      name: "FirstName",
-      title: "Enter your first name:",
-      type: "text",
+      type: "rating",
+      name: "RecommendSuunto",
+      title:
+        "How likely would you recommend Suunto to your friends and colleagues?",
+      description: "",
+      minRateDescription: "not at all",
+      maxRateDescription: "extremely likely",
+      rateMax: 10,
     },
     {
-      name: "LastName",
-      title: "Enter your last name:",
-      type: "text",
+      name: "RecommendExplanation",
+      title: "Please tell us why you gave that score.",
+      type: "comment",
+    },
+
+    {
+      type: "rating",
+      name: "OverallSatisfaction",
+      title: "Overall, how satisfied are you with Suunto?",
+      description: "",
+      minRateDescription: "not at all",
+      maxRateDescription: "extremely likely",
+      rateMax: 10,
+    },
+    {
+      type: "html",
+      name: "info",
+      html: "<div><h3>4. How much do you agree with the following statements:</h3><p>Reply from a scale from 1 to 10</p><div>",
+    },
+    {
+      type: "rating",
+      name: "Appreciation",
+      title: "Suunto shows appreciation for me as a customer",
+      rateMax: 10,
+      hideNumber: true,
+    },
+    {
+      type: "rating",
+      name: "getHelp",
+      title: "It is easy to get help from Suunto whenever I need to",
+      rateMax: 10,
+      hideNumber: true,
+    },
+    {
+      type: "rating",
+      name: "innovativeSolutions",
+      title:
+        "Suunto offers innovative solutions that make my everyday life easier",
+      rateMax: 10,
+      hideNumber: true,
+    },
+    {
+      type: "rating",
+      name: "needFit",
+      title: "Suunto’s range of products and services fit my needs",
+      rateMax: 10,
+      hideNumber: true,
+    },
+    {
+      type: "rating",
+      name: "worksWell",
+      title: "Suunto’s products work well whenever I need them",
+      rateMax: 10,
+      hideNumber: true,
+    },
+    {
+      type: "rating",
+      name: "valueeForMoney",
+      title: "Suunto provides value for money",
+      rateMax: 10,
+      hideNumber: true,
     },
   ],
 };
