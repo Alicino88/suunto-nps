63409f99a28e68b95005f2ebc2421b1282a69ffd
Alice Moretti <ali.moret88@gmail.com>
2023-03-03 15:40:38 +0200

    Made text responsive in Header

--- src/Header.js
+++ src/Header.js
@@ -17,9 +17,9 @@ const Header = () => {
             minHeight: "300px",
           }}
         />
-        <div className="d-flex position-absolute top-0 right-0 bottom-0 left-0 text-white align-items-center">
-          <div className="ms-5 ps-5">
-            <h1 className="fw-bold">HI, PARISA</h1>
+        <div className="d-flex position-absolute top-0 s-0 bottom-0 e-0 text-white align-items-center justify-content-center">
+          <div className="text-center">
+            <h1 className="fw-bold display-2">HI, PARISA</h1>
             <h2 className="fw-semibold">Thank you for buying Suunto!</h2>
             <span className="d-block border-bottom border-2 border-danger"></span>
             <p className="mt-3 fs-5 fw-semibold">We are so happy to have you</p>
