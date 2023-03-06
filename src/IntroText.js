ed78c2d493b5310367afad794097e368be3dc2be
Alice Moretti <ali.moret88@gmail.com>
2023-03-06 14:47:44 +0200

    Added breakpoints

--- src/IntroText.js
+++ src/IntroText.js
@@ -3,12 +3,13 @@ import { Container, Col } from "react-bootstrap";
 const IntroText = () => {
   return (
     <Container className="bg-light px-0 justify-content-center d-flex">
-      <Col className="pt-5 px-1 text-center">
+      <Col md={7} lg={6} className="pt-5 px-1 text-center">
         <p className="fs-6 fw-semibold">
           What do you think of using our product so far?
         </p>
         <h2 className="fs-6 mb-0">
-          Your feedback is important to us as it lets us improve
+          Your feedback is important to us as it lets us improve our products
+          and customer support.
         </h2>
       </Col>
     </Container>
